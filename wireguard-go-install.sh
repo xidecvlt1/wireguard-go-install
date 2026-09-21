0a70b5bc1b727cca76e179fdc137acf2b3a94a55
xidecvlt1 <55010061+xidecvlt1@users.noreply.github.com>
2026-09-22 03:36:02 +0700

    Remove default_qdisc and tcp_congestion_control settings
    Removed configuration for default queuing discipline and TCP congestion control.

--- wireguard-go-install.sh
+++ wireguard-go-install.sh
@@ -250,8 +250,6 @@ net.ipv4.conf.all.route_localnet=1
 net.ipv4.conf.default.route_localnet=1
 net.ipv4.conf.all.rp_filter=2
 net.ipv4.conf.default.rp_filter=2
-net.core.default_qdisc = cake
-net.ipv4.tcp_congestion_control = bbr
 EOF
 
 	sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>&1
@@ -259,8 +257,6 @@ EOF
 	sysctl -w net.ipv4.conf.default.route_localnet=1 >/dev/null 2>&1
     sysctl -w net.ipv4.conf.all.rp_filter=2 >/dev/null 2>&1
     sysctl -w net.ipv4.conf.default.rp_filter=2 >/dev/null 2>&1
-	sysctl -w net.core.default_qdisc = cake >/dev/null 2>&1
-	sysctl -w net.ipv4.tcp_congestion_control = bbr >/dev/null 2>&1
 	
 	sysctl -p /etc/sysctl.d/99-wireguard.conf >/dev/null 2>&1
 
