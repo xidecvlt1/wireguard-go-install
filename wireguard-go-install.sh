bf62ab4d920d12ce83e328fc433a7e6ae8acfa98
xidecvlt1 <55010061+xidecvlt1@users.noreply.github.com>
2026-09-21 15:47:23 +0700

    Add rp_filter settings to sysctl configuration

--- wireguard-go-install.sh
+++ wireguard-go-install.sh
@@ -248,11 +248,15 @@ EOF
 net.ipv4.ip_forward=1
 net.ipv4.conf.all.route_localnet=1
 net.ipv4.conf.default.route_localnet=1
+net.ipv4.conf.all.rp_filter=2
+net.ipv4.conf.default.rp_filter=2
 EOF
 
 	sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>&1
 	sysctl -w net.ipv4.conf.all.route_localnet=1 >/dev/null 2>&1
 	sysctl -w net.ipv4.conf.default.route_localnet=1 >/dev/null 2>&1
+    sysctl -w net.ipv4.conf.all.rp_filter=2 >/dev/null 2>&1
+    sysctl -w net.ipv4.conf.default.rp_filter=2 >/dev/null 2>&1
 	sysctl -p /etc/sysctl.d/99-amneziawg.conf >/dev/null 2>&1
 
 	if command -v netfilter-persistent &>/dev/null; then
